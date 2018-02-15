b17bf4d11a3c74aa964073b312b8a50e202e052a
Adam <adamh93@verizon.net>
2018-02-14 22:03:42 -0500

    done: Create a user profile page, setup User avatars, modify user profile page to use a sub template for user's individual posts, add to User model an aboutme and last_seen fields to be used on user profile page, use two new columns in User to be displayed on the user profile page

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,6 +5,8 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app.models import User
 from app.forms import RegistrationForm
+from datetime import datetime
+
 
 @app.route('/')
 @app.route('/index')
@@ -58,4 +60,13 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
-    
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+    {'author': user, 'body': 'Test post #1'},
+    {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
