e7508f4a5d432dfd4da37aac922ee6df121c39c9
Adam <adamh93@verizon.net>
2018-07-19 15:34:20 -0400

    done: Create a form class in forms.py, add post form to index.html template, refactor index route to handle creation and submission of post data, add to index route posts for followed user on home page

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -17,7 +17,13 @@ def before_request():
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Miguel'}
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'John'},
@@ -25,10 +31,11 @@ def index():
         },
         {
             'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool'
-        }
+            'body': 'The Avengers movie was so cool!'
+            }
     ]
-    return render_template("index.html", title='Home Page', posts=posts)
+    posts = current_user.followed_posts().all()
+    return render_template("index.html", title='Home Page', form=form, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
